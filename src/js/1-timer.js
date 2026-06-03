acd0e040e480f48b818a5f51ae6f852a40507832
NikitaShcherban <cruizsports@gmail.com>
2026-06-03 17:07:49 +0300

    Timer done

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -2,8 +2,13 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
 const buttonStart = document.querySelector('[data-start]');
+const daysCount = document.querySelector('[data-days]');
+const hoursCount = document.querySelector('[data-hours]');
+const minutesCount = document.querySelector('[data-minutes]');
+const secondsCount = document.querySelector('[data-seconds]');
 const inputDate = document.querySelector('#datetime-picker');
 let userSelectedDate = null;
+let timerId = null;
 buttonStart.disabled = true;
 
 const options = {
@@ -30,3 +35,58 @@ const options = {
 };
 
 flatpickr(inputDate, options);
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function formatDate({ days, hours, minutes, seconds }) {
+  daysCount.textContent = String(days).length < 2 ? addLeadingZero(days) : days;
+  hoursCount.textContent = addLeadingZero(hours);
+  minutesCount.textContent = addLeadingZero(minutes);
+  secondsCount.textContent = addLeadingZero(seconds);
+}
+
+function timerOn() {
+  const currentDate = Date.now();
+  const timeDif = userSelectedDate.getTime() - currentDate;
+  if (timeDif <= 0) {
+    clearInterval(timerId);
+    formatDate({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+    inputDate.disabled = false;
+    return;
+  }
+
+  const timeValues = convertMs(timeDif);
+  formatDate(timeValues);
+}
+
+function countStart(event) {
+  event.preventDefault();
+  buttonStart.disabled = true;
+  inputDate.disabled = true;
+
+  timerOn();
+  timerId = setInterval(timerOn, 1000);
+}
+
+buttonStart.addEventListener('click', countStart);
