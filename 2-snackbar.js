f78db72647386fea29e9042d65d6075666c1a4bf
nikita-shcherban <cruizsports@gmail.com>
2026-06-03 17:36:15 +0000

    Deploying to gh-pages from @ nikita-shcherban/goit-js-hw-10@d35f415db96252809c841c72e357d4959ce9063a 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{i as o}from"./assets/vendor-BbbuE1sJ.js";const s=document.querySelector(".form");s.addEventListener("submit",r);function r(){event.preventDefault();const t=Number(s.elements.delay.value),i=s.elements.state.value;new Promise((e,m)=>{setTimeout(()=>{i==="fulfilled"?e(t):m(t)},t)}).then(e=>{o.success({message:`Fulfilled promise in ${e}ms`,position:"topRight"})}).catch(e=>{o.error({message:`Rejected promise in ${e}ms`,position:"topRight"})})}
+//# sourceMappingURL=2-snackbar.js.map
